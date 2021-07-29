198d042d12378ea9bc655cd89bc3b7a8cf5daebe
CarlosCaseiroYT <82342720+CarlosCaseiroYT@users.noreply.github.com>
2021-07-29 22:29:13 +0100

    Update Constants.lua

--- ReplicatedStorage/Modules/Constants.lua
+++ ReplicatedStorage/Modules/Constants.lua
@@ -2,21 +2,20 @@ local Constants = {
 	MaxChunkDistanceVisible = 4,
 	BlockSizeMultiplier = 3,
 	AssetId = {
-		GrassTop = "rbxassetid://6967929613",
-		GrassSide = "rbxassetid://6967929535",
-		Dirt = "rbxassetid://6967929438",
-		Stone = "rbxassetid://6967929130",
-		Bedrock = "rbxassetid://6967929042",
-		WoodTopBottom = "rbxassetid://6967928951",
-		WoodSide = "rbxassetid://6967928867",
-		Leaves = "rbxassetid://6967928736",
-		WaterTop = "rbxassetid://6967929238",
-		WaterSideBottom = "rbxassetid://6967929343"
+		GrassTop = "YOUR_ASSET_ID",
+		GrassSide = "YOUR_ASSET_ID",
+		Dirt = "YOUR_ASSET_ID",
+		Stone = "YOUR_ASSET_ID",
+		Bedrock = "YOUR_ASSET_ID",
+		WoodTopBottom = "YOUR_ASSET_ID",
+		WoodSide = "YOUR_ASSET_ID",
+		Leaves = "YOUR_ASSET_ID",
+		WaterTop = "YOUR_ASSET_ID",
+		WaterSideBottom = "YOUR_ASSET_ID"
 	},
 	Chunk = {
 		XZ = 2^3,
-		Y = 2^5,
-		MaxY = 2^7
+		Y = 2^5
 	}
 }
 
